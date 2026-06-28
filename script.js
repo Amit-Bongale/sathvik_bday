98cbf76ab5d31c9cc3e540da7869ca235cdf2875
Amit-Bongale <amitbongale14@gmail.com>
2026-06-28 23:18:24 +0530

    added annimations

--- script.js
+++ script.js
@@ -21,22 +21,70 @@ setInterval(() => {
   document.getElementById("seconds").innerHTML = seconds;
 }, 1000);
 
+// Scroll Reveal
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach((section) => {
+    const top = section.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 120) {
+      section.classList.add("active");
+    }
+  });
+});
+
+// Scroll Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 500) {
+    topBtn.classList.add("show");
+  } else {
+    topBtn.classList.remove("show");
+  }
+});
+
+topBtn.onclick = () => {
+  window.scrollTo({
+    top: 0,
+
+    behavior: "smooth",
+  });
+};
+
+// Copy Venue
+
 function copyAddress() {
   navigator.clipboard.writeText("Hotel The White Lotus, Udupi");
 
-  alert("Venue copied to clipboard!");
+  const btn = event.target;
+
+  const old = btn.innerHTML;
+
+  btn.innerHTML = "Copied!";
+
+  setTimeout(() => {
+    btn.innerHTML = old;
+  }, 2000);
 }
 
+// Google Calendar
+
 function addCalendar() {
-  const title = "Sathvik Birthday Celebration";
+  const title = "SATHVIK Birthday Celebration";
 
   const location = "Hotel The White Lotus, Udupi";
+
+  const details = "Birthday Celebration";
 
   const start = "20260629T193000";
 
   const end = "20260629T223000";
 
-  const url = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${start}/${end}&location=${encodeURIComponent(location)}`;
+  const url = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${start}/${end}&details=${encodeURIComponent(details)}&location=${encodeURIComponent(location)}`;
 
   window.open(url);
 }
